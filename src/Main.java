597088484dd140190062d32ca00fb32bfddbd686
hxmai <hxmai@coremail.cn>
2018-01-19 09:31:54 +0800

    Initial test

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Main {
+    public static void main(String[] args){
+        Lock lock = new ReentrantLock();
+        ConcurrentHashMap
+    }
+}
