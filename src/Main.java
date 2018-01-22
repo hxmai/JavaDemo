94e677c84fc4fb9ee27f75ee3ee105f035f5a2d4
hxmai <hxmai@coremail.cn>
2018-01-22 09:15:01 +0800

    one

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
     public static void main(String[] args){
-        Lock lock = new ReentrantLock();
-        ConcurrentHashMap
+        User u = new User(){
+            {
+              this.name = "a";
+            }
+        };
+        ThreadLocal threadLocal = new ThreadLocal();
+        List list = new ArrayList(){
+            {
+            }
+        };
     }
+}
+
+class User{
+    public String name;
 }
